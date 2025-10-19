f960e02a490e59dcadf21daf5bbac213f44f6206
Rutuja Khatal <138274332+Rutuja2011@users.noreply.github.com>
2025-10-19 14:06:22 +0530

    Add set operations in set_operation.py
    Implement set operations including union, intersection, difference, and symmetric difference.

---
+++ set_operation.py
@@ -0,0 +1,10 @@
+#slip4_1
+#write a python program set operation : intesection, difference ,symmetric difference
+s1 = {0,2,4,6,8};
+s2 = {1,2,3,4,5};
+
+print("Union of s1 and s2 is" ,s1|s2)
+print("Intersection of s1 and s2 is", s1&s2)
+print("Difference of s1 and s2 is",s1-s2)
+print("Symmetric difference of s1 and s2 is",s1^s2)
+
